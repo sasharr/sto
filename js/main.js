6375ab7c1d08d6eeecfd7468570fe667a09b0f17
sasharr <wyra4ydo@gmail.com>
2017-06-23 12:27:02 +0300

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -181,5 +181,28 @@ $('.slider-comments').slick({
     autoplay: true,
     autoplaySpeed: 2000
 });
+
+    $("a").on('click touch', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });//smoth scroll
+
 });
 
